e6601a387e0194b2139030bf96632e5c814e3da1
Waihon Yew <yewwaihon@gmail.com>
2022-06-05 12:50:09 +0800

    Initial Commit

---
+++ Moonshot2/ContentView.swift
@@ -0,0 +1,21 @@
+//
+//  ContentView.swift
+//  Moonshot2
+//
+//  Created by Waihon Yew on 05/06/2022.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        Text("Hello, world!")
+            .padding()
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
---
+++ Moonshot2/Moonshot2App.swift
@@ -0,0 +1,17 @@
+//
+//  Moonshot2App.swift
+//  Moonshot2
+//
+//  Created by Waihon Yew on 05/06/2022.
+//
+
+import SwiftUI
+
+@main
+struct Moonshot2App: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
