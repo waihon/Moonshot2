996673a19f55f67a1f7a46bd913048a03bad57f9
Waihon Yew <yewwaihon@gmail.com>
2022-06-11 17:51:37 +0800

    Create separate structs for each level of JSON

--- Moonshot2/ContentView.swift
+++ Moonshot2/ContentView.swift
@@ -7,6 +7,16 @@
 
 import SwiftUI
 
+struct User: Codable {
+    let name: String
+    let address: Address
+}
+
+struct Address: Codable {
+    let street: String
+    let city: String
+}
+
 struct ContentView: View {
     var body: some View {
         Button("Decode JSON") {
