3755743b6581fe9845bd1b34bc36d4f04f610f63
Waihon Yew <yewwaihon@gmail.com>
2022-06-12 00:33:54 +0800

    Start with clean content view

--- Moonshot2/ContentView.swift
+++ Moonshot2/ContentView.swift
@@ -7,8 +7,14 @@
 
 import SwiftUI
 
-
 struct ContentView: View {
+    var body: some View {
+        Text("Hello, world!")
+            .padding()
+    }
+}
+
+struct HorizontalGridContentView: View {
     let layout = [
         GridItem(.adaptive(minimum: 50, maximum: 80))
     ]
