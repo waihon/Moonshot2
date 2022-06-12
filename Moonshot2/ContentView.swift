e308fd937c07203ac8346862f2a3e93bda940761
Waihon Yew <yewwaihon@gmail.com>
2022-06-12 22:29:06 +0800

    Add some simple padding to the mission images

--- Moonshot2/ContentView.swift
+++ Moonshot2/ContentView.swift
@@ -30,6 +30,7 @@ struct ContentView: View {
                                     .resizable()
                                     .scaledToFit()
                                     .frame(width: 100, height: 100)
+                                    .padding()
                                 VStack {
                                     Text(mission.displayName)
                                         .font(.headline)
