7e2edaede00b7fdcb0023eb101070fedfd62bc17
Waihon Yew <yewwaihon@gmail.com>
2022-06-12 10:26:58 +0800

    Convert crew JSON data to a CrewRole struct

---
+++ Moonshot2/Mission.swift
@@ -0,0 +1,13 @@
+//
+//  Mission.swift
+//  Moonshot2
+//
+//  Created by Waihon Yew on 12/06/2022.
+//
+
+import Foundation
+
+struct CrewRole: Codable {
+    let name: String
+    let role: String
+}
