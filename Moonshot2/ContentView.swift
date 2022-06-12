4a3eeff172412450f4ebbab4e1c8fdc836ef53dd
Waihon Yew <yewwaihon@gmail.com>
2022-06-12 21:38:26 +0800

    Take a first pass at filling in ContentView with mission badge, name, and launch date

--- Moonshot2/ContentView.swift
+++ Moonshot2/ContentView.swift
@@ -13,12 +13,37 @@ struct ContentView: View {
     // what astronauts will be.
     let astronauts: [String: Astronaut] = Bundle.main.decode("astronauts.json")
     let missions: [Mission] = Bundle.main.decode("missions.json")
+    let columns = [
+        GridItem(.adaptive(minimum: 150))
+    ]
 
     var body: some View {
-        Text("Astronauts: \(astronauts.count)")
-            .padding()
-        Text("Missions: \(missions.count)")
-            .padding()
+        NavigationView {
+            ScrollView {
+                LazyVGrid(columns: columns) {
+                    ForEach(missions) { mission in
+                        NavigationLink {
+                            Text("Detail view")
+                        } label: {
+                            VStack {
+                                Image(mission.image)
+                                    .resizable()
+                                    .scaledToFit()
+                                    .frame(width: 100, height: 100)
+                                VStack {
+                                    Text(mission.displayName)
+                                        .font(.headline)
+                                    Text(mission.launchDate ?? "N/A")
+                                        .font(.caption)
+                                }
+                                .frame(maxWidth: .infinity)
+                            }
+                        }
+                    }
+                }
+            }
+            .navigationTitle("Moonshot")
+        }
     }
 }
 
