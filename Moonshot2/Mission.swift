914292e24639984bebc46562dcc7c1973c1a32f7
Waihon Yew <yewwaihon@gmail.com>
2022-06-12 10:30:44 +0800

    Convert missions JSON data to a Mission struct

--- Moonshot2/Mission.swift
+++ Moonshot2/Mission.swift
@@ -11,3 +11,12 @@ struct CrewRole: Codable {
     let name: String
     let role: String
 }
+
+struct Mission: Codable, Identifiable {
+    let id: Int
+    // Codable will automatically skip over optionals if the values
+    // is mmising from our input JSON.
+    let launchDate: String?
+    let crew: [CrewRole]
+    let description: String
+}
